dfefc1e3fce16083c6bfb96add2f5c44fa79de20
wangfang <2404956029@qq.com>
2019-08-03 09:02:50 +0800

    umi音频视频

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from "react";
-import  "./app.css";
-import Index from './video'
-function App() {
-  return (
-        <Index />
-  );
-}
-
-export default App;
+export const dva = {
+  config: {
+    onError(err) {
+      err.preventDefault();
+      console.error(err.message);
+    },
+  },
+};
